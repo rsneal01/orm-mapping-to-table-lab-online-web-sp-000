dcc707994d87be5d43bce8e3b2ec28edb3fbf828
Bobby Neal <rsneal01@gmail.com>
2020-09-10 23:48:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,8 @@ class Student
   end 
   
   def self.create
-    student = Student.new
+    student = Student.new(name, grade)
+    
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
