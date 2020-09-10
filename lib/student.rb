6073fffe90536468831ff236e7dcf9d5e630b7d8
Bobby Neal <rsneal01@gmail.com>
2020-09-10 23:42:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
       VALUES (?, ?)
     SQL
  
-    DB[:conn].execute(sql, self.name, self.album)
+    DB[:conn].execute(sql, self.name, self.grade)
  
   end 
   # Remember, you can access your database connection anywhere in this class
