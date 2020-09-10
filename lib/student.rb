930b721a6b8783cbae2f147c5edcb3d2ebbf499b
Bobby Neal <rsneal01@gmail.com>
2020-09-10 23:33:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, age, id = nil)
+  def initialize(name, grade, id = nil)
     @name = name
     @age = age
     @id = id
