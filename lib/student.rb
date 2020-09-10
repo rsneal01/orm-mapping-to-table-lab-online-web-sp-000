1015ec2f61928968243d45411c3e82d55588297f
Bobby Neal <rsneal01@gmail.com>
2020-09-10 23:28:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,16 @@
 class Student
+  
+  attr_accessor :name, :age
+  attr_reader :id
 
+  def initialize(name, age, id = nil)
+    @name = name
+    @age = age
+    @id = id
+  end
+  
+  
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
